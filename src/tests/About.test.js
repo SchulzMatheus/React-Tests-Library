feb864961954cd5849052d3bb068bcc6a611abe4
Matheus Simplicio <matheusschulz47@gmail.com>
2022-11-28 22:28:30 -0300

    req 1 & 2 done

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,33 @@
-test('', () => {});
-//42
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './Utils/renderWithRouter';
+import About from '../pages/About';
+
+describe('Testa o componente <About.js />', () => {
+  it('Verifica se o header está correto', () => {
+    renderWithRouter(<About />);
+    const header = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(header).toBeInTheDocument();
+  });
+
+  it('Verifica se contém as informações corretas;', () => {
+    renderWithRouter(<About />);
+    const about = screen.getByText(/This application simulates a Pokédex/i);
+    expect(about).toBeInTheDocument();
+  });
+
+  it('A página deve conter dois paragrafos com informações', () => {
+    renderWithRouter(<About />);
+    const p1 = screen.getByText(/This application simulates a Pokédex/i);
+    const p2 = screen.getByText(/One can filter Pokémon by type/i);
+    expect(p1).toBeInTheDocument();
+    expect(p2).toBeInTheDocument();
+  });
+
+  it('A pagina deve conter uma imagem', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByRole('img', { name: /pokédex/i });
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './Utils/renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente <App.js />', () => {
+  it('O botão home é exibido na tela', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: /home/i });
+    expect(home).toBeInTheDocument();
+  });
+
+  it('O botão about é exibido na tela', () => {
+    renderWithRouter(<App />);
+    const about = screen.getByRole('link', { name: /about/i });
+    expect(about).toBeInTheDocument();
+  });
+
+  it('O botão favorite pokémon é exibido na tela', () => {
+    renderWithRouter(<App />);
+    const fav = screen.getByRole('link', { name: /favorite pokémon/i });
+    expect(fav).toBeInTheDocument();
+  });
+});
