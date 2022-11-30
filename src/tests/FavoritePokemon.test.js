6a4454a2764e2995db7f62ab6741c0ba658b3299
Matheus Simplicio <matheusschulz47@gmail.com>
2022-11-30 18:20:59 -0300

    req 3?

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './Utils/renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemon';
+import App from '../App';
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  it('Testa se é exibido os favoritos', () => {
+    renderWithRouter(<App />);
+    const link = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(link);
+    const favorite = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+    userEvent.click(favorite);
+    const favoriteLink = screen.getByRole('link', { name: /Favorite Pokémon/i });
+    userEvent.click(favoriteLink);
+    const checkPokemon = screen.getByText(/Pikachu/i);
+    expect(checkPokemon).toBeInTheDocument();
+  });
+
+  it('Testa se quando a lista de fav é vazia retorna uma string especifica', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const checkMessage = screen.getByText(/No favorite Pokémon found/i);
+    expect(checkMessage).toBeInTheDocument();
+  });
+});
