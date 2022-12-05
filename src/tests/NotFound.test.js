f08661c68197b285e47dae82882e2d335c6c88f1
Matheus Simplicio <matheusschulz47@gmail.com>
2022-12-05 08:50:43 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Testa o componente NotFound', () => {
+  it('Testa se o gif é redenrizado', () => {
+    const { getByRole } = render(<NotFound />);
+    const image = getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(image).toBeInTheDocument();
+  });
+  it('Testa se contém texto correto', () => {
+    const { getByRole } = render(<NotFound />);
+    const header = getByRole('heading', { name: /page requested not found/i });
+    expect(header).toBeInTheDocument();
+  });
+});
